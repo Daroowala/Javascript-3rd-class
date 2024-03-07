368c875359915a7431fe130a103882d7d0877b3a
my name <blueshaperd3@gmail.com>
2024-03-08 01:39:54 +0500

    indexof

---
+++ app.js
@@ -0,0 +1,24 @@
+// var a = 10;
+// if (a !== 5){
+//     document.write("Correct")
+// }
+// else{
+//     document.write("Incorrect")
+// }
+
+
+
+var password = prompt("Enter password");
+
+if (password.indexOf(" ") !== -1) {
+  document.write("password can not contain spaces");
+} else {
+  if (password.indexOf(".") === -1) {
+document.write("password must contain .");
+  }
+  if (password.length < 8) {
+    document.write("password must contain atleast 8 characters");
+  } else {
+    document.write("correct password");
+  }
+}
