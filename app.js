006e0477d5dfc9647ce5b8432fd89a02acf4052d
my name <blueshaperd3@gmail.com>
2024-03-11 01:11:14 +0500

    Arrays

--- app.js
+++ app.js
@@ -8,17 +8,21 @@
 
 
 
-var password = prompt("Enter password");
+//var password = prompt("Enter password");
 
-if (password.indexOf(" ") !== -1) {
-  document.write("password can not contain spaces");
-} else {
-  if (password.indexOf(".") === -1) {
-document.write("password must contain .");
-  }
-  if (password.length < 8) {
-    document.write("password must contain atleast 8 characters");
-  } else {
-    document.write("correct password");
-  }
-}
+//if (password.indexOf(" ") !== -1) {
+  //document.write("password can not contain spaces");
+//} else {
+  //if (password.indexOf(".") === -1) {
+//document.write("password must contain .");
+  //}
+  //if (password.length < 8) {
+    //document.write("password must contain atleast 8 characters");
+  //} else {
+    //document.write("correct password");
+  //}
+// }
+
+ var students = ["Rehan", "Ali", "Asif", "Aamir"]
+ alert("Welcome to class" + (" ") + students[1])
+ document.write("Not Welcome to "  + students[0])
